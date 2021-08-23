c9af0c5cc65d9dc46a1598c55c0e026ca8135bf4
Dariusz Filipiak <dariusz.filipiak@igt.com>
2021-08-23 17:58:31 +0200

    new version 0.0.81

--- src/config.ts
+++ src/config.ts
@@ -333,8 +333,8 @@ function pathResolved(...partOfPath: string[]) {
         if (global['firedev-upgrade-process']) {
           try {
 
-            child_process.execSync(`git reset --hard && git pull origin master`,
-              { cwd: morphiPathUserInUserDir });
+            // child_process.execSync(`git reset --hard && git pull origin master`,
+            //   { cwd: morphiPathUserInUserDir });
             // fse.removeSync(path.join(path.dirname(morphiPathUserInUserDir), 'morphi/.vscode'));
 
           } catch (error) {
