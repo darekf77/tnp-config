3ee76c6a9cf8ba5eafda51cad141bded50d044e8
Dariusz Filipiak <dariusz.filipiak@igt.com>
2023-01-03 08:41:42 +0100

    before slim version

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -768,6 +768,7 @@ export const config = {
   required: {
     npm: [
       { name: '@angular/cli', version: '13' },
+      { name: '@vercel/ncc', version: '0.36.0' },
       { name: 'extract-zip', version: '1.6.7' },
       { name: 'watch', version: '1.0.2' },
       { name: 'cpr' },
