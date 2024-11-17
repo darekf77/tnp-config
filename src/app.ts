83f7345a01b1ef38090d606577e8f88a371bdcc0
Dariusz Filipiak <darekf77@gmail.com>
2024-11-17 07:52:07 +0100

    release:  v18.0.16

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,27 @@
-import { Helpers } from "tnp-core/src";
+import { Helpers } from 'tnp-core/src';
 
-console.log('nothing here')
+console.log('nothing here');
 
 export function start() {
-
-  Helpers.info('helo')
-
+  Helpers.info('helo');
 }
 
 export default start;
+
+//#region  tnp-config component
+//#region @browser
+@Component({ template: 'hello world fromr tnp-config' })
+export class TnpConfigComponent {}
+//#endregion
+//#endregion
+
+//#region  tnp-config module
+//#region @browser
+@NgModule({
+  declarations: [TnpConfigComponent],
+  imports: [CommonModule],
+  exports: [TnpConfigComponent],
+})
+export class TnpConfigModule {}
+//#endregion
+//#endregion
