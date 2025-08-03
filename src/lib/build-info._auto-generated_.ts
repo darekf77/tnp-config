564f5fdd40182e32156738f240b4869beb3a3499
Dariusz Filipiak <darekf77@gmail.com>
2025-08-03 21:22:26 +0200

    release: v19.0.37

--- src/lib/build-info._auto-generated_.ts
+++ src/lib/build-info._auto-generated_.ts
@@ -1,7 +1,8 @@
 // THIS FILE IS GENERATED - DO NOT MODIFY
 export const BUILD_FRAMEWORK_CLI_NAME = 'tnp';
 export const APP_ID = 'com.domain.example.tnp-config';
+export const BUILD_BASE_HREF = '';
 export const PROJECT_NPM_NAME = 'tnp-config';
-export const CURRENT_PACKAGE_VERSION = '19.0.36';
+export const CURRENT_PACKAGE_VERSION = '19.0.37';
 // THIS FILE IS GENERATED - DO NOT MODIFY
       
